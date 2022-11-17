f26f8baced81e8d1260bd8fa9d28e924774d7c00
Gereltuya <gereltuya@Gereltuyas-MacBook-Pro.local>
2022-11-17 20:08:51 +0800

    set up board game api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,23 @@
 import * as express from 'express';
-import { Message } from '@bghoard/api-interfaces';
+import { getGames } from './app/game';
+import { createReview, getReviews } from './app/review';
+import { addItemToCart, getCart, updateItemInCart } from './app/store';
 
 const app = express();
+app.use(express.json());
 
-const greeting: Message = { message: 'Welcome to api!' };
+// list of games
+app.get('/api/game', getGames);
 
-app.get('/api', (req, res) => {
-  res.send(greeting);
-});
+// reviews for the game
+app.get('/api/review/:game', getReviews);
+// create review for the game
+app.post('/api/review/:game', createReview);
+
+// get all cart, post, update
+app.get('/api/cart', getCart);
+app.post('/api/cart', addItemToCart);
+app.put('/api/cart', updateItemInCart);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
